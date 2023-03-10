a35fd4f872a1fff0c7e6f8a2a659266d40f3a9ef
Barbara da Paixão Perez Rodrigues <barbara220040@ilum.cnpem.br>
2023-03-09 22:39:53 -0300

    Adicionei os notebooks do experimento A.01 e A.02

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,9 @@
- 
+def funcao_objetivo_caixa_binaria(individuo):
+    """ Computa a função objetivo no problema das caixas binárias.
+    
+    Args: 
+        Individuo: lista contendo os genes das caixas binárias.
+    Return:
+    Um valor representando a soma dos genes do individuo.
+    """
+    return sum(individuo) 
